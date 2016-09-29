8b3614f1264d83af6749f9a410890c33615c2525
xe57473 <franciscojavier.alvarez.contractor@bbva.com>
2016-09-29 17:44:02 +0200

    feat(): first version of the portal. scroll header panel with 'keep condensed header' and a right drawer panel. responsive. everything with polymer elements.

---
+++ src/aluminium-app/aluminium-app.js
@@ -0,0 +1,18 @@
+(function() {
+
+  'use strict';
+
+  Polymer({
+
+      is: 'aluminium-app',
+
+      properties: {
+        prop1: {
+          type: String,
+          value: 'aluminium-app',
+        },
+      },
+
+    });
+
+}());
